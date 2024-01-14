309d04b02cd6ceddddce88a075cc6fc27f2e40cc
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-14 06:56:02 +0200

    0. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,4 +5,8 @@ def canUnlockAll(boxes):
     """
     method that determines if all the boxes can be opened
     """
+    for k in boxes[0][1:]:
+        if k not in boxes[1:]:
+            return False  
+    return True
     
