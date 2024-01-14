d79b637f31134a6cb7b520017ab8e62575e5b8a1
THEKINGSTAR <thekingstar5002@gmail.com>
2024-01-14 06:57:50 +0200

    pycondestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Lockboxes"""
 
+
 def canUnlockAll(boxes):
     """
     method that determines if all the boxes can be opened
     """
     for k in boxes[0][1:]:
         if k not in boxes[1:]:
-            return False  
+            return False
     return True
-    
