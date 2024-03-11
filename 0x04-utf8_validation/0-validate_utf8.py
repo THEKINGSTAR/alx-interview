65db61e29553512c08c45b80df450ed8d44ec1a8
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-11 18:39:13 +0200

    update the repo

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+method that determines if a given data set
+represents a valid UTF-8 encoding
+"""
+
+def validUTF8(data):
+    """
+    Return: True if data is a valid UTF-8 encoding,
+    else
+    return False
+    """
