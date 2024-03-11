35043d40a7bba14694a7100caebe4ddeea0162f1
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-11 18:38:21 +0200

    minimize the code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,25 +15,14 @@ def pascal_triangle(n):
     if (n <= 0):
         return (pascal)
     else:
-        # Use a loop to iterate through each row
-        # from 0 to the desired number of rows (n).
-        for row in range(n):
-            # For each row, initialize a list (row)
-            # with 1 repeated i + 1 times
-            # where i is the current row index.
-            row_list = [1] * (row + 1)
-
-            # Use another loop to calculate the values
-            # inside the row.
-            # Start the loop from the second element
-            # (j = 1) up to the second-to-last element (i - 1).
-            for v in range(1, row):
-                # Set row[j] to the sum of the values
-                # from the previous
-                # row: triangle[i - 1][j - 1] + triangle[i - 1][j].
-                row_list[v] = pascal[row - 1][v - 1] + pascal[row - 1][v]
-
-            # Append the completed row to the list storing Pascal's Triangle.
-            pascal.append(row_list)
+        if n == 1:
+            pascal = [1]
+            return pascal
+        else:
+            for row in range(n):
+                rw_lst = [1] * (row + 1)
+                for c in range(1, row):
+                    rw_lst[c] = pascal[row - 1][c - 1] + pascal[row - 1][c]
+                pascal.append(rw_lst)
 
     return (pascal)
