e96647a3f9a5bfcbb32b57b13b6b76db98907c23
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-29 00:11:53 +0200

    try to address the output

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,16 +2,26 @@
 """
 Write a script that reads stdin line by line and computes metrics:
 """
+
+
+import sys
+
+
 file_size = 0
-status_code =[200, 301, 400, 401, 403, 404, 405, 500]
+status_code = [200, 301, 400, 401, 403, 404, 405, 500]
 date = ''
 IP_Address = ''
 total_size = 0
 
-# Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+# Input format:
+# <IP Address>
+# -
+# [<date>]
+# "GET /projects/260 HTTP/1.1"
+# <status code>
+# <file size>
 
 
-import sys
 print('Write a message and press Enter: ')
 for line in sys.stdin:
     line = line.rstrip()
