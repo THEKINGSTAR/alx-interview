5291249445f10caa2c5c27ddea92671f366bc146
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-23 02:28:31 +0200

    the final version 0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -11,8 +11,8 @@ const https = require('https');
 // COMMAND LINE ARGUMENTS
 const movieId = process.argv[2];
 if (!movieId) {
-    console.error('Please provide a movie ID as the first argument.');
-    process.exit(1);
+  console.error('Please provide a movie ID as the first argument.');
+  process.exit(1);
 }
 
 // API URL
@@ -20,56 +20,56 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 // Function to fetch character data
 const fetchCharacterData = characterUrl => {
-    return new Promise((resolve, reject) => {
-        https.get(characterUrl, (response) => {
-            let data = '';
+  return new Promise((resolve, reject) => {
+    https.get(characterUrl, (response) => {
+      let data = '';
 
-            // A chunk of data has been received.
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
+      // A chunk of data has been received.
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
 
-            // The whole response has been received.
-            response.on('end', () => {
-                const character = JSON.parse(data);
-                resolve(character.name);
-            });
-        }).on("error", (error) => {
-            reject(error);
-        });
+      // The whole response has been received.
+      response.on('end', () => {
+        const character = JSON.parse(data);
+        resolve(character.name);
+      });
+    }).on('error', (error) => {
+      reject(error);
     });
+  });
 };
 
 // Main function to fetch movie data and character names
 const fetchMovieData = () => {
-    https.get(url, (response) => {
-        let data = '';
+  https.get(url, (response) => {
+    let data = '';
 
-        // A chunk of data has been received.
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received.
-        response.on('end', () => {
-            const film = JSON.parse(data);
-
-            // Array to store promises for character data
-            const characterPromises = film.characters.map(characterUrl => fetchCharacterData(characterUrl));
-
-            // Wait for all character data promises to resolve
-            Promise.all(characterPromises)
-                .then(characterNames => {
-                    // Print character names in correct order
-                    characterNames.forEach(name => console.log(name));
-                })
-                .catch(error => {
-                    console.error('Error fetching character data:', error);
-                });
-        });
-    }).on("error", (error) => {
-        console.error('Error fetching movie data:', error);
+    // A chunk of data has been received.
+    response.on('data', (chunk) => {
+      data += chunk;
     });
+
+    // The whole response has been received.
+    response.on('end', () => {
+      const film = JSON.parse(data);
+
+      // Array to store promises for character data
+      const characterPromises = film.characters.map(characterUrl => fetchCharacterData(characterUrl));
+
+      // Wait for all character data promises to resolve
+      Promise.all(characterPromises)
+        .then(characterNames => {
+          // Print character names in correct order
+          characterNames.forEach(name => console.log(name));
+        })
+        .catch(error => {
+          console.error('Error fetching character data:', error);
+        });
+    });
+  }).on('error', (error) => {
+    console.error('Error fetching movie data:', error);
+  });
 };
 
 // Call the main function
